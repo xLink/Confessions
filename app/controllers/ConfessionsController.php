5f2374f3c06c90893cad85d59acda53889263bc7
John Maguire <contact@johnmaguire.me>
2013-12-18 03:55:23 -0500

    removes unnecessary edit/update methods from ConfessionsController

--- app/controllers/ConfessionsController.php
+++ app/controllers/ConfessionsController.php
@@ -66,28 +66,6 @@ class ConfessionsController extends BaseController {
 	}
 
 	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
-
-	/**
 	 * Remove the specified resource from storage.
 	 *
 	 * @param  int  $id
