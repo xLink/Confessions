c45757ef53127500c05b5b17bfb23bebd7929677
John Maguire <contact@johnmaguire.me>
2013-12-18 04:49:20 -0500

    adds csrf checks on confession submission and deletion

--- app/routes.php
+++ app/routes.php
@@ -15,6 +15,6 @@ Route::post('signup', ['as' => 'users.store', 'uses' => 'UsersController@store',
 /* Confessions routes */
 Route::get('confessions', ['as' => 'confessions.index', 'uses' => 'ConfessionsController@index']);
 Route::get('confessions/create', ['as' => 'confessions.create', 'uses' => 'ConfessionsController@create', 'before' => 'auth']);
-Route::post('confessions', ['as' => 'confessions.store', 'uses' => 'ConfessionsController@store', 'before' => 'auth']);
+Route::post('confessions', ['as' => 'confessions.store', 'uses' => 'ConfessionsController@store', 'before' => 'auth|csrf']);
 Route::get('confessions/{id}', ['as' => 'confessions.show', 'uses' => 'ConfessionsController@show']);
-Route::delete('confessions/{id}', ['as' => 'confessions.destroy', 'uses' => 'ConfessionsController@destroy', 'before' => 'auth']);
+Route::delete('confessions/{id}', ['as' => 'confessions.destroy', 'uses' => 'ConfessionsController@destroy', 'before' => 'auth|csrf']);
