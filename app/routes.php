ab12203f3178a991bc629b9533cd40825895492a
John Maguire <contact@johnmaguire.me>
2013-12-18 03:53:32 -0500

    makes routes more explicit and adds filters

--- app/routes.php
+++ app/routes.php
@@ -5,12 +5,14 @@ Route::get('/', ['as' => 'home', 'uses' => 'HomeController@showHome']);
 
 /* Auth routes */
 Route::get('login', ['as' => 'login', 'uses' => 'SessionsController@create', 'before' => 'guest']);
+Route::post('login', ['as' => 'sessions.store', 'uses' => 'SessionsController@store', 'before' => 'guest']);
 Route::get('logout', ['as' => 'logout', 'uses' => 'SessionsController@destroy']);
-Route::resource('sessions', 'SessionsController', ['only' => ['create', 'store', 'destroy']]);
 
 /* User routes */
 Route::get('signup', ['as' => 'signup', 'uses' => 'UsersController@create', 'before' => 'guest']);
-Route::resource('users', 'UsersController', ['only' => ['create', 'store']]);
+Route::post('signup', ['as' => 'users.store', 'uses' => 'UsersController@store', 'before' => 'guest']);
 
 /* Confessions routes */
-Route::resource('confessions', 'ConfessionsController');
+Route::get('confessions', ['as' => 'confessions.index', 'uses' => 'ConfessionsController@index']);
+Route::get('confessions/create', ['as' => 'confessions.create', 'uses' => 'ConfessionsController@create', 'before' => 'auth']);
+Route::post('confessions', ['as' => 'confessions.store', 'uses' => 'ConfessionsController@store', 'before' => 'auth']);
