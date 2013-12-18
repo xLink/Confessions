51844f0884f1de453323767c057d10c078091bed
John Maguire <contact@johnmaguire.me>
2013-12-18 04:52:10 -0500

    adds max length validators for username and email

--- app/Confessions/Services/Validation/UserValidator.php
+++ app/Confessions/Services/Validation/UserValidator.php
@@ -3,8 +3,8 @@
 class UserValidator extends Validator {
 
 	static $rules = array(
-		'username'         => 'required|unique:users|not_in:Anonymous,anonymous',
-		'email'            => 'required|email|unique:users',
+		'username'         => 'required|max:20|unique:users|not_in:Anonymous,anonymous',
+		'email'            => 'required|max:255|email|unique:users',
 		'password'         => 'required',
 		'confirm_password' => 'required|same:password',
 	);
