a4f1bfa711ff204d171cbc47701564d8b7b5111a
John Maguire <contact@johnmaguire.me>
2013-12-19 12:32:41 -0500

    adds group posted to info in confession lists

--- app/views/_partials/confession.blade.php
+++ app/views/_partials/confession.blade.php
@@ -1,5 +1,5 @@
 <blockquote style='margin-left: 13px'>{{{ $confession->body }}}</blockquote>
-<p style='margin-left: 45px'>&mdash; <i>{{{ ($confession->anonymous) ? 'Anonymous' : $confession->user->username }}}</i>, <a href="{{ route('confessions.show', ['id' => $confession->id]) }}" title="Permalink to this confession">{{ $confession->created_at->diffForHumans() }}</a></p>
+<p style='margin-left: 45px'>&mdash; <i>{{{ ($confession->anonymous) ? 'Anonymous' : $confession->user->username }}}</i>, <a href="{{ route('confessions.show', ['id' => $confession->id]) }}" title="Permalink to this confession">{{ $confession->created_at->diffForHumans() }}</a>@if($confession->group), in <a href="{{ route('groups.show', ['id' => $confession->group->id]) }}">{{{ $confession->group->name }}}</a>.@endif</p>
 @if($user->id == $confession->user_id)
 	{{ Form::open(array('route' => array('confessions.destroy', $confession->id), 'method' => 'delete')) }}
 		{{ Form::submit('Delete this confession', array('class' => 'btn btn-sm btn-danger', 'style' => 'margin-left: 45px')) }}
