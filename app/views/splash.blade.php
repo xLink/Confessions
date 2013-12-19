3601aff5116d7139ab1dafdfac4553077d07765b
John Maguire <contact@johnmaguire.me>
2013-12-19 17:18:54 -0500

    fixing some links on the splash page

--- app/views/splash.blade.php
+++ app/views/splash.blade.php
@@ -6,10 +6,10 @@
 
 		<p>This is a website where anyone can anonymously submit confessions or stories, either to the public feed or in a localized group.</p>
 
-		<p><a class="btn btn-primary btn-lg" href-"#">Get started</a></p>
+		<p><a class="btn btn-primary btn-lg" href="{{ route('signup') }}">Get started</a></p>
 	</div>
 
 	<p>This project was born out of the belief of free speech &mdash; submit confessions about anything without fear of retribution.</p>
 
-	<p>Along the same vein, we believe that <a href="http://en.wikipedia.org/wiki/Information_wants_to_be_free">"information wants to be free"</a> and as such this entire project is licensed under the MIT open source license. If you're interested in checking out how the site works, you may view the code on <a href="#">Github</a>.</p>
+	<p>Along the same vein, we believe that <a href="http://en.wikipedia.org/wiki/Information_wants_to_be_free">"information wants to be free"</a> and as such this entire project is licensed under the MIT open source license. If you're interested in checking out how the site works, you may view the code on <a href="https://github.com/JohnMaguire2013/Confessions">Github</a>.</p>
 @stop
