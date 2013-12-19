e56111c6f55d58b8d2d0b1c26fffeaca20e955a3
John Maguire <contact@johnmaguire.me>
2013-12-19 12:15:46 -0500

    makes confession displaying a bit more efficient

--- app/views/_partials/confession.blade.php
+++ app/views/_partials/confession.blade.php
@@ -1,6 +1,6 @@
 <blockquote style='margin-left: 13px'>{{{ $confession->body }}}</blockquote>
 <p style='margin-left: 45px'>&mdash; <i>{{{ ($confession->anonymous) ? 'Anonymous' : $confession->user->username }}}</i>, <a href="{{ route('confessions.show', ['id' => $confession->id]) }}" title="Permalink to this confession">{{ $confession->created_at->diffForHumans() }}</a></p>
-@if($user->id == $confession->user->id)
+@if($user->id == $confession->user_id)
 	{{ Form::open(array('route' => array('confessions.destroy', $confession->id), 'method' => 'delete')) }}
 		{{ Form::submit('Delete this confession', array('class' => 'btn btn-sm btn-danger', 'style' => 'margin-left: 45px')) }}
 	{{ Form::close() }}
